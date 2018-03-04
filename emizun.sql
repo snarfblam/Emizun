b338d2785aab760057343e8bcda91b7c4c4ce08d
Tom H <snarfblam@gmail.com>
2018-03-04 18:22:12 -0500

    Addition of departments table and product_sales column on products

--- emizun.sql
+++ emizun.sql
@@ -9,6 +9,13 @@ CREATE TABLE products (
     stock_quantity INTEGER(11),
     PRIMARY KEY (item_id)
 );
+
+ALTER TABLE products
+ADD COLUMN product_sales INTEGER(11) AFTER stock_quantity;
+UPDATE products SET product_sales='0';
+UPDATE products SET product_sales='0' WHERE item_id >= 0;
+
+
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('1', 'ShamWow', 'Home & Kitchen', '1995', '20');
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('2', 'Snuggie', 'Home & Kitchen', '1980', '50');
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('3', 'Chia Pet', 'Garden & Outdoor', '1597', '30');
@@ -19,3 +26,12 @@ INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('8', 'Mighty Putty', 'Tools & Home Improvement', '1549', '60');
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('9', 'Flex Seal', 'Tools & Home Improvement', '1299', '30');
 INSERT INTO `emizun`.`products` (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`) VALUES ('10', 'Music Bullet', 'Electronics', '2499', '40');
+
+CREATE TABLE departments (
+    department_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(255) NOT NULL,
+    overhead_costs INTEGER(11) NOT NULL,
+    PRIMARY KEY (department_id)
+);
+
+INSERT INTO emizun.departments (department_name, overhead_costs) VALUES ('Health & Wellness', '10000');
