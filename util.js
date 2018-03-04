816865b6a303d0701235debd543ebb6750dc5482
Tom H <snarfblam@gmail.com>
2018-03-04 12:49:30 -0500

    util.displayTable

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 var inquirer = require('inquirer');
 
 /** Formats a string to use columns of the specified widths. */
-function tableize(values, widths, columnSeparator) {
+function tableize(values, widths, columnSeparator,) {
     var result = "";
 
     for (var i = 0; i < values.length; i++) {
@@ -22,14 +22,81 @@ function tableize(values, widths, columnSeparator) {
     return result;
 }
 
-function tableSeparator(columnWidths, columnSeparator) {
+function tableSeparator(columnWidths, columnSeparator, separatorChar) {
     var result = "";
     for (var i = 0; i < columnWidths.length; i++) {
         if (i > 0) result += columnSeparator;
-        result += ('-').repeat(columnWidths[i]);
+        result += (separatorChar || '-').repeat(columnWidths[i]);
     }
 
     return result;
+}
+
+/** Formats a table of values.
+ * @param {string[][] | any[]} data - An array of rows. Each row is an array of strings for each column or an object that will be converted to an array of strings (see outputFunction).
+ * @param {number[]} columnLayout - An array of numbers specifying column widths. A default value will be used if the array is too small for the number of needed columns.
+ * @param {Object} [tableStyle] - An object defining the appearance of the table. See tableStyles object.
+ * @param {(item: any) => string[]} [dataTransform] - A function that converts each object in the data array into an array of column values
+ * @param {(text: string) => void} [outputFunction] - A function that is called successively with each line of the rendered table. Defaults to console.log.
+ */
+function displayTable(data, columnLayout, tableStyle, dataTransform, outputFunction) {
+    // Use default table style if not specified
+    var finalStyle = tableStyle || tableStyles.dashes;
+
+    // Log to console by default
+    outputFunction = outputFunction || _console_log;
+
+    // doet
+    outputFunction(
+        finalStyle.topLeft +
+        tableSeparator(columnLayout, finalStyle.intersectionTop, finalStyle.separator) + 
+        finalStyle.topRight);
+    
+    data.forEach(function (item) {
+        if (dataTransform) item = dataTransform(item);
+        outputFunction(
+            finalStyle.left + 
+            tableize(item, columnLayout, finalStyle.columnSeparator, outputFunction) +
+            finalStyle.right);
+    });
+
+    outputFunction(
+        finalStyle.bottomLeft +
+        tableSeparator(columnLayout, finalStyle.intersectionBottom, finalStyle.separator) + 
+        finalStyle.bottomRight);
+}
+
+function _console_log(string) {
+    console.log(string);
+}
+
+var tableStyles = {
+    dashes: {
+        separator: '-',
+        columnSeparator: ' | ',
+        intersectionTop: '-+-',
+        intersectionMiddle: '-+-',
+        intersectionBottom: '-+-',
+        topLeft: "",
+        left: "",
+        bottomLeft: "",
+        topRight: "",
+        right: "",
+        bottomRight: "",
+    },
+    boxDrawing: {
+        separator: '─',
+        columnSeparator: ' │ ',
+        intersectionTop: '─┬─',
+        intersectionMiddle: '─┼─',
+        intersectionBottom: '─┴─',
+        topLeft: "┌─",
+        left: "│ ",
+        bottomLeft: "└─",
+        topRight: "─┐",
+        right: " │",
+        bottomRight: "─┘",
+    }
 }
 
 /** Formats a number of cents into a string such as $1.99 */
@@ -101,6 +168,8 @@ function Prompt() {
 module.exports.tableize = tableize;
 module.exports.tableSeparator = tableSeparator;
 module.exports.formatCurrency = formatCurrency;
+module.exports.displayTable = displayTable;
+module.exports.tableStyles = tableStyles;
 // module.exports.prompt = function () { return new Prompt(); }
 module.exports.prompt = {
     input: function (name, message) { return new Prompt().input(name, message); },
