c6ce540de656b4d0c5da47e73f1ff13427a726da
Thomas Hudson <snarfblam@gmail.com>
2018-03-05 20:58:08 -0500

    Added EmizunConnection to util

--- Emizun-Util/emizunUtil.js
+++ Emizun-Util/emizunUtil.js
@@ -1,7 +1,9 @@
 var prompt = require('./prompt');
 var table = require('./table');
+var EmizunConnection = require('./EmizunConnection');
 
 module.exports = {
     prompt: prompt,
     table: table,
+    EmizunConnection: EmizunConnection,
 }
